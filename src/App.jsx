2bd46e70aed5e994436e3dec49a047927ba50225
Trilokinath Chaubey <chaubeytn2103@outlook.com>
2024-10-05 21:49:44 +0530

    React Form Made with useRef ���

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from 'react';
-import Form from './components/Form';
+import { useState, useRef } from 'react';
 function App() {
 
+  const name = useRef(null);
+  const age = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name.current.value);
+    console.log(age.current.value);
+  }
+
   return (
-    <Form />
+    <form action="" onSubmit={handleSubmit}>
+      <input type="text" ref={name} placeholder="Name" />
+      <input type="text" ref={age} placeholder="Age" />
+      <input type="submit" />
+    </form>
   )
 }
 
